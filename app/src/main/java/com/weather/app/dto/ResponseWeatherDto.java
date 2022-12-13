d81430483ed751d8589d4cba68f6e9c2293df05e
paweljawa <pawelbojaryn@gmail.com>
2022-12-13 18:35:55 +0100

    Deleted more comments.

--- app/src/main/java/com/weather/app/dto/ResponseWeatherDto.java
+++ app/src/main/java/com/weather/app/dto/ResponseWeatherDto.java
@@ -1,25 +1,4 @@
 package com.weather.app.dto;
-/*
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-
-import java.util.List;
-
-@Getter
-public class ResponseWeatherDto {
-    @JsonProperty("city_name")
-    private String cityName;
-    @JsonProperty("data")
-    private List<WeatherDto> weatherDtoList;
-
-    public ResponseWeatherDto() {
-    }
-
-    public ResponseWeatherDto(String cityName, List<WeatherDto> weatherDtoList) {
-        this.cityName = cityName;
-        this.weatherDtoList = weatherDtoList;
-    }
-}*/
 
 import java.util.List;
 
